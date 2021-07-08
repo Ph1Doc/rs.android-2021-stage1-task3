0fbe0c0ba22835f447ab4adaefd2d5256a44aded
Ph1Doc <oleg.sapun@gmail.com>
2021-07-08 18:43:26 +0300

    feat: add func for Combinator

--- src/main/kotlin/subtask1/Combinator.kt
+++ src/main/kotlin/subtask1/Combinator.kt
@@ -4,6 +4,32 @@ class Combinator {
 
     // TODO: Complete the following function
     fun checkChooseFromArray(array: Array<Int>): Int? {
-        throw NotImplementedError("Not implemented")
+
+        val numberOfPosters = array[0]
+        val numberOfColors = array[1]
+
+        if (numberOfColors == numberOfPosters) {
+            return 1
+        } else {
+            for (i in 1 until numberOfColors) {
+                val n = permutations(numberOfColors)
+                val nr = permutations(numberOfColors - i) * permutations(i)
+                val c = n/nr
+
+                if (c >= numberOfPosters) {
+                    return i
+                }
+            }
+        }
+
+        return null
+    }
+
+    private fun permutations(number: Int): Double {
+        var perm = 1.0
+        for (i in 1..number) {
+            perm *= i
+        }
+        return perm
     }
 }
