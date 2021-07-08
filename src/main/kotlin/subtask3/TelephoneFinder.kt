61cb8d6b513d21b28c664ac8af2ce4833b338f11
Ph1Doc <oleg.sapun@gmail.com>
2021-07-08 22:42:32 +0300

    feat: add func for Telephone Finder

--- src/main/kotlin/subtask3/TelephoneFinder.kt
+++ src/main/kotlin/subtask3/TelephoneFinder.kt
@@ -4,6 +4,36 @@ class TelephoneFinder {
 
     // TODO: Complete the following function
     fun findAllNumbersFromGivenNumber(number: String): Array<String>? {
-        throw NotImplementedError("Not implemented")
+
+        val keypad =
+           mapOf( "0" to arrayOf("8"),
+            "1" to arrayOf("2", "4"),
+            "2" to arrayOf("1", "3", "5"),
+            "3" to arrayOf("2", "6"),
+            "4" to arrayOf("1", "5", "7"),
+            "5" to arrayOf("2", "4", "6", "8"),
+            "6" to arrayOf("3", "5", "9"),
+            "7" to arrayOf("4", "8"),
+            "8" to arrayOf("5", "7", "9", "0"),
+            "9" to arrayOf("6", "8"))
+
+        if (number.toInt() < 0) {
+            return null
+        } else {
+            val numbers = mutableListOf<String>()
+            for (i in number.indices) {
+                val neighbours = keypad[number.substring(i,i+1)]
+
+                if (neighbours != null) {
+                    for (element in neighbours) {
+                        val chars = number.toCharArray()
+                        chars[i] = element[0]
+                        val newNeighbour = String(chars)
+                        numbers.add(newNeighbour)
+                        }
+                }
+            }
+            return Array(numbers.size) { i -> numbers[i] }
+        }
     }
 }
