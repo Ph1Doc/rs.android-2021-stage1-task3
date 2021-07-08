ab8117163228797d02840a50f487c62ebcecaf6c
Ph1Doc <oleg.sapun@gmail.com>
2021-07-08 21:15:06 +0300

    feat: add func to Square Decomposer

--- src/main/kotlin/subtask1/Combinator.kt
+++ src/main/kotlin/subtask1/Combinator.kt
@@ -21,7 +21,6 @@ class Combinator {
                 }
             }
         }
-
         return null
     }
 
--- src/main/kotlin/subtask2/SquareDecomposer.kt
+++ src/main/kotlin/subtask2/SquareDecomposer.kt
@@ -4,6 +4,31 @@ class SquareDecomposer {
 
     // TODO: Complete the following function
     fun decomposeNumber(number: Int): Array<Int>? {
-        throw NotImplementedError("Not implemented")
+
+        return if (number < 5) {
+            null
+        } else {
+            val result = findSquaresFor(number.toDouble(), number*number.toDouble())
+            Array(result.size - 1) { i -> result[i] }
+        }
+    }
+
+    private fun findSquaresFor(number: Double, square: Double): Array<Int> {
+
+        var j = (number - 1)
+        if (square == 0.0) {
+            return arrayOf(number.toInt())
+        }
+
+        while (j > 0) {
+            if (square - j * j >= 0) {
+                val squares = findSquaresFor(j, (square - j * j))
+                if (squares.isNotEmpty()) {
+                    return squares + arrayOf(number.toInt())
+                }
+            }
+            j--
+        }
+        return arrayOf()
     }
 }
